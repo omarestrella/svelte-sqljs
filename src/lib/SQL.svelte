1d250edc0235efc08355d92c25cf9a7756fabf68
Omar Estrella <oyestrella91@gmail.com>
2022-02-17 21:19:10 -0500

    Store and load array buffer from indexeddb

--- src/lib/SQL.svelte
+++ src/lib/SQL.svelte
@@ -2,21 +2,39 @@
   import initSqlJS from "sql.js";
   import sqlWasmUrl from "sql.js/dist/sql-wasm.wasm?url";
   import { onMount } from "svelte";
+  import localforage from "localforage";
 
-  onMount(async () => {
+  localforage.config({
+    name: "sqljs",
+  });
+
+  async function createDatabase() {
     const SQL = await initSqlJS({
       locateFile: () => sqlWasmUrl,
     });
 
+    const keys = await localforage.keys();
+    if (keys.includes("database")) {
+      const currentData = await localforage.getItem("database");
+      return new SQL.Database(currentData as Uint8Array);
+    }
+
     const db = new SQL.Database();
-    let sqlstr =
-      "CREATE TABLE hello (a int, b char); \
-  INSERT INTO hello VALUES (0, 'hello'); \
-  INSERT INTO hello VALUES (1, 'world');";
+    let sqlstr = `CREATE TABLE hello (a int, b char);
+    INSERT INTO hello VALUES (0, 'hello');
+    INSERT INTO hello VALUES (1, 'world');
+    `;
     db.run(sqlstr);
+    await localforage.setItem("database", db.export());
+    return db;
+  }
+
+  onMount(async () => {
+    const db = await createDatabase();
+
     const stmt = db.prepare("SELECT * FROM hello WHERE a=:aval AND b=:bval");
-    // Bind values to the parameters and fetch the results of the query
     const result = stmt.getAsObject({ ":aval": 1, ":bval": "world" });
     console.log(result);
+    db.close();
   });
 </script>
